3a41d65f3fdc08f1c9ff655fa45b714904fe2f2f
S L D C Karunarathne <dutheeshwork@gmail.com>
2025-03-31 10:32:18 +0530

    Initial Spring Boot setup for Travel Connect

---
+++ backend/src/main/java/com/travelconnect/backend/TravelConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travelconnect.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelConnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelConnectBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/travelconnect/backend/TravelConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelconnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelConnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
