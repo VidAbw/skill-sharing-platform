44a0ed2e71c851fdc40a1c09fecbb1aea850c22e
VidAbw <vidushaupashaly@gmail.com>
2025-04-02 08:37:07 +0530

    Initial commit

---
+++ learning-updates/src/main/java/com/skillshare/learning_updates/LearningUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.skillshare.learning_updates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningUpdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningUpdatesApplication.class, args);
+	}
+
+}
---
+++ learning-updates/src/test/java/com/skillshare/learning_updates/LearningUpdatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillshare.learning_updates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningUpdatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
