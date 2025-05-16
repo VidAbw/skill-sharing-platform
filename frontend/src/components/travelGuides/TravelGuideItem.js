4aa1c917f6d34db31b0016c2550ef7b8cd887b8b
VidAbw <vidushaupashaly@gmail.com>
2025-05-17 01:45:40 +0530

    Guide item added CSS

--- frontend/src/components/travelGuides/TravelGuideItem.js
+++ frontend/src/components/travelGuides/TravelGuideItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/TravelGuide.css';
+import './TravelGuideList.css';
 
 const TravelGuideItem = ({ guide, onDelete }) => {
   return (
