4015be7709e3ca76c6d0eb2990ea108b2709dd9b
VidAbw <vidushaupashaly@gmail.com>
2025-05-16 15:14:48 +0530

    TravelGuide itme delete function added

--- frontend/src/components/travelGuides/TravelGuideItem.js
+++ frontend/src/components/travelGuides/TravelGuideItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/TravelGuide.css';
 
-const TravelGuideItem = ({ guide }) => {
+const TravelGuideItem = ({ guide, onDelete }) => {
   return (
     <div className="guide-card">
       <h3>{guide.title}</h3>
-      <p className="guide-destination">Destination: {guide.destination}</p>
-      {guide.topic && <p className="guide-topic">Topic: {guide.topic}</p>}
+      <p className="guide-destination"><em>Destination:</em> {guide.destination}</p>
+      {guide.topic && <p className="guide-topic"><em>Topic:</em> {guide.topic}</p>}
       <p className="guide-description">
         {guide.description.length > 150
           ? `${guide.description.substring(0, 150)}...`
@@ -16,6 +16,11 @@ const TravelGuideItem = ({ guide }) => {
       <Link to={`/travel-guides/${guide.id}`} className="view-more-link">
         View Details
       </Link>
+      {onDelete && (
+        <button className="delete-guide-btn" onClick={() => onDelete(guide.id)}>
+          Delete
+        </button>
+      )}
     </div>
   );
 };
